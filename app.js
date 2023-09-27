a2b88b5d9bc233c2c949231b0c64b458c70c460f
Jonathan_Kiprono <kmutai315@gmail.com>
2023-09-27 13:07:34 +0300

    Additional files added

---
+++ app.js
@@ -0,0 +1,63 @@
+//function declarations
+
+function printMe(){
+    console.log('jonathan')
+}
+printMe();
+
+function mathFun(){
+    let a = 3;
+    let b = 4 ;
+    console.log( a + b);
+}
+mathFun();
+
+//integers with strings.
+
+function elevatorOne (){
+    const x = 37;
+    const b = 12;
+    const c = " code loves Jonathan"
+    console.log(x + b + c)
+    
+}
+elevatorOne();
+
+//Arguments vs parameters
+
+function printThis(param){
+    console.log(param);
+}
+printThis('tapas');
+
+
+//Expressions
+
+function onePlus(){
+    console.log('best');
+}
+onePlus();
+
+const onePlusOne = function(){
+    console.log('second best')
+
+}
+onePlusOne();
+//Return
+function x (){
+    console.log('morning');
+
+}
+x ();
+
+/*const p = x();*/
+
+function y (){
+    console.log("good");
+}
+y();
+
+function sumAll( m, n){
+    return m + n;
+  
+}
